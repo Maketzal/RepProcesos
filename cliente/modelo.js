87b3181ddc74e3b258c1168c50057c91a4702442
CARLOS MAQUEDA ZABALLOS <Carlos.Maqueda1@alu.uclm.es>
2023-09-25 18:49:38 +0200

    Terminada implementacion arquitectura base

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -1,14 +1,31 @@
 function Sistema(){
     this.usuarios={};
     this.agregarUsuario=function(nick){
+        if(nick in this.usuarios){
+            console.log("Usuario ya existente");
+        }
+        else console.log("Usuario añadido correctamente");
     this.usuarios[nick]=new Usuario(nick);
     }
     this.obtenerUsuarios=function(){
+        console.log(this.usuarios)
         return this.usuarios;
         }
     this.usuarioActivo=function(nick){
         return (nick in this.usuarios);
     }
+    this.eliminarUsuario=function(nick){
+        if(nick in this.usuarios){
+            delete(this.usuarios[nick]);
+
+            console.log("Usuario eliminado");
+        }
+        else console.log("Usuario no encontrado");
+    }
+    this.numeroUsuarios=function(){
+        return Object.keys(this.usuarios).length
+    }
+    
    }
    function Usuario(nick){
     this.nick=nick;
